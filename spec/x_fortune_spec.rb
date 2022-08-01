0a57659ddb4b2c995c8f23a117c83849074fa42a
unurgunite <senpaiguru1488@gmail.com>
2022-08-01 18:46:39 +0300

    Refactored specs

--- spec/x_fortune_spec.rb
+++ spec/x_fortune_spec.rb
@@ -1,13 +1,22 @@
 require './x_fortune'
 require 'rspec'
+require 'rspec/collection_matchers'
 
 RSpec.describe FortuneQuote do
-  let(:quote) { described_class.new(`/usr/bin/env fortune -n 153`) }
+  let(:quote) { described_class.new(`/usr/bin/env fortune -n 153`).chomp }
 
   describe '.generate_quoute' do
+    it '' do
+      expect(quote).to be_a(String)
+    end
+
+    it 'should be an instance of String' do
+      #binding.irb
+      expect(quote).to have_at_least(1).characters
+    end
+
     it 'should return short quote' do
-      expect(quote).not_to include(/\r\n|\r|\n/).at_least(3).times
+      expect(quote).to include(/\r\n|\r|\n/).at_most(3).times
     end
   end
 end
-
